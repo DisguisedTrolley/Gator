f5380f374b694c44c5ed4586b8e9f276c0ffb6b9
Samarth Bhat <samarthrbhat52@gmail.com>
2025-04-27 18:26:03 +0530

    refactored handler file

--- handler_users.go
+++ handler_users.go
@@ -66,14 +66,3 @@ func handlerListUsers(s *state, cmd command) error {
 
 	return nil
 }
-
-func handlerReset(s *state, _ command) error {
-	err := s.db.DeleteUsers(context.Background())
-	if err != nil {
-		return fmt.Errorf("error resetting database")
-	}
-
-	fmt.Println("Database reset successful")
-
-	return nil
-}
